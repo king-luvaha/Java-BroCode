9ce5b6036c5fece567d7e486c0b53eb1c40eae3f
Luvaha <luvaha456@gmail.com>
2026-07-16 18:39:55 +0300

    Introduction to Java

---
+++ src/Main.java
@@ -0,0 +1,27 @@
+public class Main {
+
+    public static void main (String[] args){
+
+        // Comments
+        // This is my first java program
+
+        /*
+
+        This is a multiline comment.
+        This
+        too
+        is
+        a
+        multiline
+        comment
+         */
+
+        System.out.println("I like ugali!");
+        System.out.println("It's really delicious!");
+        System.out.println("I eat it with chicken!");
+        System.out.println("Sometimes with nyama choma!");
+        System.out.println("And a lot of kachumbari!");
+
+    }
+
+}
